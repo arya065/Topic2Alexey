f3d8fe0d79f0322c523fda18b2ba6c0854e83e75
Alexey <arya065@g.educaand.es>
2022-10-06 11:11:15 +0200

    ej07

---
+++ src/main/java/variables/Ej07.java
@@ -0,0 +1,21 @@
+
+
+package variables;
+
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class Ej07 {
+
+    public static void main(String[] args) {
+        
+        double b,c;
+        
+        String a=JOptionPane.showInputDialog("Enter the number of euros",100);
+        c=Double.parseDouble(a);
+        
+        b=c*0.99;
+        JOptionPane.showMessageDialog(null, b+" USD");
+        
+    }
+}
