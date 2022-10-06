6a1eb5a32833e0474b749c983c03b97155a7633b
Alexey <arya065@g.educaand.es>
2022-10-06 16:42:56 +0200

    ej11

--- src/main/java/variables/Ej11.java
+++ src/main/java/variables/Ej11.java
@@ -11,14 +11,17 @@ public class Ej11 {
         
         Double s1,s2,S;
                 
-        String A=JOptionPane.showInputDialog("Enter the value of R");
-        String B=JOptionPane.showInputDialog("Enter the value of H");
+        String A=JOptionPane.showInputDialog("Enter the value of Radius");
+        String B=JOptionPane.showInputDialog("Enter the value of side H");
         
         Double r=Double.parseDouble(A);
         Double h=Double.parseDouble(B);
         
-        s1= Math.PI*r;
-        s2= Math.pow(h, 2);
+        s1= Math.PI*Math.pow(r,2)/2;
+        s2= (Math.sqrt(Math.pow(h, 2)-Math.pow(r,2)))*r;
+        System.out.println(s1);
+        System.out.println(s2);
+        
         S=s1+s2;
         JOptionPane.showMessageDialog(null, "The area of the room is "+S);
     }
