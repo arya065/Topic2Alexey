1402841220a822cb7b8eef22c7421c5454bd12da
Alexey <arya065@g.educaand.es>
2022-10-06 10:49:07 +0200

    ej06

---
+++ src/main/java/variables/Ej06.java
@@ -0,0 +1,31 @@
+
+
+package variables;
+
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class Ej06 {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        double a,b,c,summ;
+        
+        System.out.println("Enter the first part");
+        a=in.nextDouble();
+        System.out.println("Enter the second part");
+        b=in.nextDouble();
+        System.out.println("Enter the third part");
+        c=in.nextDouble();
+        
+        summ=a+b+c;
+        double percent1= a%summ;
+        double percent2= b%summ;
+        double percent3= c%summ;
+        
+        System.out.printf("The percent of 1-st person is %.3f \n",percent1);
+        System.out.printf("The percent of 2-nd person is %.3f \n",percent2);
+        System.out.printf("The percent of 3-rd person is %.3f",percent3);
+    }
+}
